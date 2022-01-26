68ef0c04acb93677d6d0722a36c6e5fa760b0e47
1239185705@qq.com <1239185705@qq.com>
2022-01-26 14:38:31 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zs1995/RebornGoProject
+
+go 1.16
---
+++ say.go
@@ -0,0 +1,8 @@
+package RebornGoProject
+
+import "fmt"
+
+// say Hi to someone
+func SayHi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
